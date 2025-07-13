44c6a33457a6e3a13c0b4134865a75c5fa9eacab
Mustafa Safri <safrimustafa@gmail.com>
2025-07-13 13:25:06 -0700

    [Wake Code] fix quietcool.h

--- components/quiet_cool/fan/quietcool.h
+++ components/quiet_cool/fan/quietcool.h
@@ -61,7 +61,7 @@ class QuietCool {
     // REMOTE_ID is now the name for the unique remote identifier
 
   public:
-    QuietCool(uint8_t csn, uint8_t gdo0, uint8_t gdo2, uint8_t sck, uint8_t miso, uint8_t mosi, const uint8_t* remote_id_in, float freq_mhz, float deviation_khz, wake_code);
+    QuietCool(uint8_t csn, uint8_t gdo0, uint8_t gdo2, uint8_t sck, uint8_t miso, uint8_t mosi, const uint8_t* remote_id_in, float freq_mhz, float deviation_khz, uint8_t wake_code);
     void begin();
     void send(QuietCoolSpeed speed, QuietCoolDuration duration);
 };
